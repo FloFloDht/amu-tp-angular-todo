48a40f0a719206bd80f48e68b89a2a7ce850fec4
FloFloDht <flo.11.italia@gmail.com>
2022-01-13 11:11:56 +0100

    connexion supabase et récuperation des taches via requetes http

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 // src/app/app.component.ts 
 
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Tasks } from './types/task';
 
 @Component({
@@ -21,12 +23,33 @@ import { Tasks } from './types/task';
   `,
   styles: []
 })
-export class AppComponent {
-  tasks: Tasks = [
-    { id: 1, text: "Aller faire des courses", done: false },
-    { id: 2, text: "Faire à manger", done: true },
-  ];
 
+
+export class AppComponent {
+  tasks: Tasks = [];
+
+  // Grâce au constructor, on peut indiquer à Angular que notre
+  // composant aura besoin d'une instance de la classe HttpClient
+
+  constructor(private http : HttpClient){
+    // Ayant obtenu l'instance de HttpClient, on peut l'utiliser
+    // pour appeler Supabase en méthode GET. On peut tout de suite
+    // indiquer à la méthode GET qu'elle doit s'attendre à recevoir un json
+    // correspondant à un tableau de tâches (le fameux type Tasks).
+    // On n'oubliera pas aussi de préciser pour cette requête HTTP
+    // les entêtes importantes comme le Content-Type ou la clé API
+    this.http.get<Tasks>('https://bfxuknbmubeqdrtgqmrj.supabase.co/rest/v1/todosReact', {
+      headers: {
+        "Content-type" : "application.json",
+        apiKey : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTgyNDk3MywiZXhwIjoxOTU3NDAwOTczfQ.KabUwP4iOJcH6PsOedizvsT3lrX4LSh7KDelwznXE3E"
+      }
+    })
+    // Lorsque la requête aura terminé son travail et que le serveur
+    // aura répondu, nous recevrons une liste de tâches que
+    // nous pourrons alors assigner à notre propriété "tasks"
+    .subscribe((tasks) => this.tasks = tasks)
+  }
+  
   // La méthode addTask recevra une string
   addTask(text: string) {
     // Elle s'en servira pour créer une nouvelle tâche dans 
--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TaskFormComponent } from './task-form.component';
@@ -14,7 +15,8 @@ import { TodoListComponent } from './todo-list.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
